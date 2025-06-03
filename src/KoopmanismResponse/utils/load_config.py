cc28d9d36ab7c0730cc42ea2227a30aae106596c
Niccolo Zagli <niccolo.zagli@su.se>
2025-06-03 09:25:13 +0200

    Arnold Map has been introduced and the integration works. The invariant measure has been verified

--- src/KoopmanismResponse/utils/load_config.py
+++ src/KoopmanismResponse/utils/load_config.py
@@ -6,7 +6,11 @@ Holds the functions to load the settings specified in `config.py`.
 
 from functools import lru_cache
 
-from KoopmanismResponse.config import EdmdFourierSettings, OneDimensionalMapSettings
+from KoopmanismResponse.config import (
+    ArnoldMapSettings,
+    EdmdFourierSettings,
+    OneDimensionalMapSettings,
+)
 
 
 @lru_cache
@@ -15,6 +19,14 @@ def get_one_dimensional_model_settings() -> OneDimensionalMapSettings:
     Loads the settings for the One dimensional Map.
     """
     return OneDimensionalMapSettings()
+
+
+@lru_cache
+def get_Arnold_model_settings() -> ArnoldMapSettings:
+    """
+    Loads the settings for the generalised Arnold Map.
+    """
+    return ArnoldMapSettings()
 
 
 @lru_cache
